042231f76763faa028b5796acb171564022a6da1
Robin Dietrich <robin.dietrich@tum.de>
2020-11-24 20:14:03 +0100

    Added navigator node and uncertainty to sim node

--- pcimr_simulation/src/robot_sim/discrete_robot_sim.py
+++ pcimr_simulation/src/robot_sim/discrete_robot_sim.py
@@ -4,12 +4,13 @@ from robot_sim.bresenham import bresenham
 import numpy as np
 
 
-MOVE_IDS = {'N': 0, 'S': 1, 'W': 2, 'E': 3}
+MOVE_IDS = {'N': 0, 'E': 1, 'S': 2, 'W': 3}
+MOVE_PROB_ALLOC = [0, -1, +1, -2]
 
 MOVE = [(0, 1),     # Up
+        (1, 0),     # Right
         (0, -1),    # Down
-        (-1, 0),    # Left
-        (1, 0)]     # Right
+        (-1, 0)]    # Left
 
 
 class RobotSimulator2D:
@@ -17,7 +18,9 @@ class RobotSimulator2D:
     A class that defines a simple environment for a robot including its interaction with it (sensing/moving).
     """
 
-    def __init__(self, world: np.ndarray = None, pos: list = None, size_x: int = None, size_y: int = None, num_objects: int = None, num_sensors: int = 4):
+    def __init__(self, world: np.ndarray = None, pos: list = None, size_x: int = None, size_y: int = None,
+                 num_objects: int = None, num_sensors: int = 4,
+                 move_variance: np.ndarray = np.array([0.9, 0.04, 0.04, 0.0, 0.02])):
         """
         Initializes the class RobotEnvironment.
 
@@ -25,9 +28,15 @@ class RobotSimulator2D:
         @param size_y: The size of the world in y direction.
         @param num_objects: The number of objects.
         @param num_sensors: The number of sensors (4 or 8), defaults to 4.
+        @param move_variance: The variance of the movement. Defaults to [0.9, 0.04, 0.04, 0.0, 0.02]
+                                                            -> [prob. that it moved in expected direction,
+                                                                prob. that it moved left of the intended direction,
+                                                                prob. that it moved right of the intended direction,
+                                                                prob. that it moved backwards,
+                                                                prob. that it stayed in position]
         """
         if world is None:
-            self.world = np.zeros(size_x, size_y)
+            self.world = np.zeros((size_x, size_y), dtype=np.int8)
             self.size_x = size_x
             self.size_y = size_y
 
@@ -47,6 +56,7 @@ class RobotSimulator2D:
             self.num_sensors = 4
         self.num_sensors = num_sensors
 
+        self.move_variance = move_variance
 
     def init_world(self, num_objects: int):
         """
@@ -74,11 +84,11 @@ class RobotSimulator2D:
         @param position: The initial position of the robot (x, y).
         @return: True if cell at given position is valid (free), False otherwise (occupied).
         """
-
         if position is None:
-            self.pos = np.array([np.random.randint(self.size_x), np.random.randint(self.size_y)])
-            while self.world[tuple(self.pos)] > 0:
-                self.pos = np.array([np.random.randint(self.size_x), np.random.randint(self.size_y)])
+            pos = np.array([np.random.randint(self.size_x), np.random.randint(self.size_y)])
+            while self.world[tuple(pos)] != 0 or np.all(self.pos == pos):
+                pos = np.array([np.random.randint(self.size_x), np.random.randint(self.size_y)])
+            self.pos = pos
         else:
             if not self.cell_valid(*position):
                 return False
@@ -140,23 +150,29 @@ class RobotSimulator2D:
         @param move_dir: The direction in which the robot is supposed to move (N, S, E, W)
         @return: True if the move was successful, False otherwise.
         """
+
         if move_dir is None:
             # Generate random movement while generated move is not valid
             move_id = np.random.randint(len(MOVE))
 
-            while not self.cell_in_world(self.pos[0] + MOVE[move_id][0], self.pos[1] + MOVE[move_id][0]) \
-                    or self.world[self.pos[0] + MOVE[move_id][0], self.pos[1] + MOVE[move_id][0]] > 0:
+            while not self.move_valid(move_id):
                 move_id = np.random.randint(len(MOVE))
         
         else:
             move_id = MOVE_IDS[move_dir.upper()]
 
-            if not self.cell_in_world(self.pos[0] + MOVE[move_id][0], self.pos[1] + MOVE[move_id][1]) \
-                    or self.world[self.pos[0] + MOVE[move_id][0], self.pos[1] + MOVE[move_id][1]] > 0:
+            while not self.move_valid(move_id):
                 return False
 
-        self.pos[0] += MOVE[move_id][0]
-        self.pos[1] += MOVE[move_id][1]
+        # Generate real movement based on command and move variance
+        new_move_id = self.generate_prob_move(move_id)
+        while not self.move_valid(new_move_id):
+            new_move_id = self.generate_prob_move(move_id)
+        if new_move_id == -1:
+            return True
+
+        self.pos[0] += MOVE[new_move_id][0]
+        self.pos[1] += MOVE[new_move_id][1]
 
         return True
 
@@ -180,3 +196,34 @@ class RobotSimulator2D:
         @return: True, if the cell lies within the world and is free, False otherwise.
         """
         return self.cell_in_world(x, y) and self.world[x, y] == 0
+
+    def move_valid(self, move_id) -> bool:
+        """
+        Checks whether the move command with the given id would result in a cell that is located within the boundaries
+        of the world and is not occupied (free).
+
+        @param move_id: The id of the move direction.
+        @return: True, if the cell lies within the world and is free, False otherwise.
+        """
+        return self.cell_in_world(self.pos[0] + MOVE[move_id][0], self.pos[1] + MOVE[move_id][1]) \
+                    and self.world[self.pos[0] + MOVE[move_id][0], self.pos[1] + MOVE[move_id][1]] == 0
+
+    def generate_prob_move(self, move_id) -> int:
+        value = np.random.rand()
+        total_prob = 0
+        prob_id = -1
+
+        for i, prob in enumerate(self.move_variance):
+            total_prob += prob
+            if value <= total_prob:
+                prob_id = i
+                break
+
+        if prob_id == len(self.move_variance)-1:
+            move_id = -1
+        else:
+            move_id += MOVE_PROB_ALLOC[prob_id]
+            move_id += len(MOVE)
+            move_id = move_id%len(MOVE)
+
+        return move_id
